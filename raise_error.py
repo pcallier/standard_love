38736dfef35a31bf9b5d534692b9563bdf6af388
patrickc <pcallier@iqt.org>
2016-10-17 18:22:14 -0700

    Update assets

---
+++ raise_error.py
@@ -0,0 +1,12 @@
+def a(*args, **kwargs):
+    b(*args, **kwargs, error=True)
+    
+def b(*args, **kwargs):
+    c(*args, **kwargs)
+    
+def c(*args, **kwargs):
+    d(*args, **kwargs)
+
+def d(error=False, *args, **kwargs):
+    if error:
+        raise Exception("Error condition!")
